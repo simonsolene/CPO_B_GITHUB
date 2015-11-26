0cafc0ddb164d10bd7cc0ee57e2dc4789e221cd6
simonsolene <solene.jeanson@u-psud.fr>
2015-11-26 11:35:45 +0100

    :)

--- TD3_CPOB/src/edu/iut/app/IApplicationLog.java
+++
@@ -1,8 +0,0 @@
-package edu.iut.app;
-
-public interface IApplicationLog {
-	public void setMessage(String message);
-	public String getMessage();
-	public void addListener(IApplicationLogListener listener);
-	public IApplicationLogListener[] getApplicationLogListeners();
-}
--- TD3_CPOB/src/edu/iut/app/IApplicationLogListener.java
+++
@@ -1,5 +0,0 @@
-package edu.iut.app;
-
-public interface IApplicationLogListener {
-	public void newMessage(String level, String message);
-}
